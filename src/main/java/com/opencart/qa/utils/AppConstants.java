d00512de53ca34b569c2066954a883dcaf10ed86
Srilakshmi Nimmala <srilakshmi.nimmala@gmail.com>
2026-04-10 07:37:09 -0500

    corrected appconstants for errors

--- src/main/java/com/opencart/qa/utils/AppConstants.java
+++ src/main/java/com/opencart/qa/utils/AppConstants.java
@@ -14,11 +14,11 @@ public class AppConstants {
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String LOGIN_PAGE_URL = "route=account/login";
 
-	public static final String HOME_PAGE_TITLE = "My Account11";
+	public static final String HOME_PAGE_TITLE = "My Account";
 
 	
 	
-	public static final List<String> EXP_HEADERS_LIST = Arrays.asList("My Account11", "My Orders", "My Affiliate Account", "Newsletter");
+	public static final List<String> EXP_HEADERS_LIST = Arrays.asList("My Account", "My Orders", "My Affiliate Account", "Newsletter");
 	
 	public static final String REGISTER_SUCCESS_MESSG = "Your Account Has Been Created!";
 	
